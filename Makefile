d8a547c71b1bd2445d483862fe88494e40f922ee
George Lima <georgelima11@hotmail.com>
2020-05-20 13:41:08 -0300

    chore: remove lib_dirs

--- Makefile
+++ Makefile
@@ -4,9 +4,4 @@ all:
 	cd ion-c &&./build-release.sh
 	cp -R ./ion-c/ionc/include/ionc ./c_src/qldbex/ion
 	cp -R ./ion-c/decNumber/include/decNumber ./c_src/qldbex/decNumber
-	mkdir ./c_src/qldbex/c_libs
-	cp -R ./ion-c/build/release/decNumber/ ./c_src/qldbex/c_libs/decNumber
-	cp -R ./ion-c/build/release/ionc/ ./c_src/qldbex/c_libs/ionc
 
-clean:
-	rm -rf ./ion-c ./c_src/qldbex/ion ./c_src/qldbex/decNumber ./c_src/qldbex/c_libs
--- bundlex.exs
+++ bundlex.exs
@@ -18,16 +18,8 @@ defmodule Qldbex.BundlexProject do
           "qldbex.c"
         ],
         libs: ["decNumber", "ionc", "json-c", "crypto"],
-        includes: ["c_src/qldbex"],
-        lib_dirs: [
-          get_lib_path("decNumber"),
-          get_lib_path("ionc")
-        ]
+        includes: ["c_src/qldbex"]
       ]
     ]
-  end
-
-  defp get_lib_path(folder) do
-    Path.join(__DIR__, "c_src/qldbex/c_libs/#{folder}")
   end
 end
